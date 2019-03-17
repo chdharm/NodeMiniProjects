730fcce007b0661adf2942b1db52b949cda0b573
Dharmpal <dharmpalchaudhary@airasia.com>
2019-03-17 18:32:08 +0530

    starting with first episode

---
+++ ep-1/read_file.js
@@ -0,0 +1,11 @@
+var fs=require("fs");
+
+fs.readFile(__filename,{encoding:'utf-8'},gotFileContent);
+
+function gotFileContent(err,content){
+    if(err){
+        console.error(err);
+    }else{
+        console.log(content);
+    }
+}
---
+++ ep-1/temp/target.js
@@ -0,0 +1,28 @@
+var fs=require("fs");
+var path=require("path");
+var dir=path.join(__dirname,'temp')
+var source=__filename;
+var target=path.join(dir,'target.js');
+fs.mkdir(dir,function(err){
+    if(err){
+        handleError(err);
+    }else{
+        //Means folder creation is successful
+        fs.readFile(source,{encoding:'utf-8'},function(err,content){
+            if(err){
+                handleError(err);
+            }else{
+                fs.writeFile(target,content,function(err){
+                    if(err){
+                        handleError("The operation is unsuccessful !");
+                    }else{
+                        console.log("The operation is successful !");
+                    }
+                });
+            }
+        });
+    }
+});
+function handleError(e){
+    console.log(e)
+}
